5e3223da47ea0a0392d27c662528c824e312788d
Thomas Nachtergaele <thomas.nachtergaele@worldline.com>
2018-02-05 12:50:05 +0100

    reorg spec files

--- src/test/scala/hashcode/qualification/ProblemSpec.scala
+++ src/test/scala/hashcode/qualification/ProblemSpec.scala
@@ -5,13 +5,8 @@ import org.scalatest.junit.JUnitRunner
 import org.scalatest.{FlatSpec, Matchers}
 
 @RunWith(classOf[JUnitRunner])
-class ValidatorSpec extends FlatSpec with Matchers {
+class ProblemSpec extends FlatSpec with Matchers {
   val problem = Parser.read("qualification/example.in")
-  val sol = Solution(Vector(
-    ServerAffectation(0, Vector(2)),
-    ServerAffectation(1, Vector(3, 1)),
-    ServerAffectation(2, Vector(0, 1))
-  ))
 
   //  "Validator" should "score trivial problem/solution" in {
   //    val sol = Solution(Vector(
