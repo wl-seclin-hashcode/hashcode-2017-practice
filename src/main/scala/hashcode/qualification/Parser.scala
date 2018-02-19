9d34bed61d277564e75ca6fce70fff28b4429795
[a518291] Michel Daviot <michel.daviot@atos.net>
2018-02-19 16:48:13 +0100

    :construction: trying to re-read json of the Problem

--- src/main/scala/hashcode/qualification/Parser.scala
+++ src/main/scala/hashcode/qualification/Parser.scala
@@ -10,56 +10,62 @@ import core._
 import json._
 import jsonBackends.jawn._
 import formatters.humanReadable._
+import rapture.data.Serializer
 
 object Parser {
+  implicit val ser = implicitly[Serializer[Endpoint, Json]]
+//  implicit val ext = implicitly[Extractor[Endpoint, Json]]
 
 
-  def read(f: String): Problem = {
+  def read(f: String): Problem =
+//    if (path(f).toFile.exists) {
+//      val content = io.Source.fromFile(path(f).toFile).getLines.mkString
+//      val p = Json.parse(content).as[Problem]
+//      ???
+//    }else
+    {
+      val scan = new Scanner(new File(f))
 
-    val scan = new Scanner(new File(f))
+      val v = scan.nextInt()
+      val e = scan.nextInt()
+      val r = scan.nextInt()
+      val c = scan.nextInt()
+      val x = scan.nextInt()
 
-    val v = scan.nextInt()
-    val e = scan.nextInt()
-    val r = scan.nextInt()
-    val c = scan.nextInt()
-    val x = scan.nextInt()
-
-    scan.nextLine()
-
-    val videoSizes = scan.nextLine().split(" ").map(_.toInt).toVector
-
-    def readEndpoint(id: Int): Endpoint = {
-      val l = scan.nextInt()
-      val k = scan.nextInt()
       scan.nextLine()
-      val latencies = (for {
-        i <- 0 until k
-        line = scan.nextLine()
-        Array(cacheId, lat) = line.split(" ").map(_.toInt)
-      } yield cacheId -> lat).toMap
-      Endpoint(id, l, latencies)
+
+      val videoSizes = scan.nextLine().split(" ").map(_.toInt).toVector
+
+      def readEndpoint(id: Int): Endpoint = {
+        val l = scan.nextInt()
+        val k = scan.nextInt()
+        scan.nextLine()
+        val latencies = (for {
+          i <- 0 until k
+          line = scan.nextLine()
+          Array(cacheId, lat) = line.split(" ").map(_.toInt)
+        } yield cacheId -> lat).toMap
+        Endpoint(id, l, latencies)
+      }
+
+      val endpoints = Vector.tabulate(e)(readEndpoint)
+
+      val requests = Vector.fill(r) {
+        val Array(v, e, n) = scan.nextLine().split(" ").map(_.toInt)
+        Request(v, e, n)
+      }
+
+      val p = Problem(c, x, videoSizes, endpoints, requests)
+
+      val serialized = Json(p).toString
+      Files.write(path(f), serialized.getBytes(StandardCharsets.UTF_8))
+
+      p
+
     }
 
-    val endpoints = Vector.tabulate(e)(readEndpoint)
-
-    val requests = Vector.fill(r) {
-      val Array(v, e, n) = scan.nextLine().split(" ").map(_.toInt)
-      Request(v, e, n)
-    }
-
-    val p = Problem(c, x, videoSizes, endpoints, requests)
-    implicit val ser = implicitly[Serializer[Endpoint, Json]]
-    val serialized = Json(ProblemJson(c, x, videoSizes, endpoints, requests)).toBareString
-    Files.write(Paths.get(f + ".json"), serialized.getBytes(StandardCharsets.UTF_8))
-
-    p
-
+  private def path(f: String) = {
+    Paths.get(f + ".json")
   }
-
-  case class ProblemJson(c: Int, x: Int,
-                         videoSizes: Vector[Int],
-                         endpoints: Vector[Endpoint],
-                         reqs: Vector[Request]
-                        )
 
 }
